bac6616ae89b786ad62f0c78a4dad7de31e56c1b
naruto1031 <naruto0614@au.com>
2023-03-24 17:15:04 +0900

    受信及び送信機能の実装完了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useEffect, useState } from 'react';
 import { GetStaticProps, NextPage } from 'next';
 import { client } from '../../libs/client';
+import axios from 'axios';
+
 
 interface Chat {
   id: string;
@@ -8,7 +10,7 @@ interface Chat {
   updatedAt: string;
   publishedAt: string;
   revisedAt: string;
-  use_name: string;
+  user_name: string;
   chat_content: string;
 }
 
@@ -21,6 +23,11 @@ interface ChatListResponse {
   totalCount: number;
   offset: number;
   limit: number;
+}
+
+interface SubmitContents {
+  user_name: string;
+  chat_content: string;
 }
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
@@ -37,16 +44,16 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   };
 };
 
-const Chat: FC<{ chat: Chat }> = ({ chat }) => {
+const Chat: FC<{ chat_elem: Chat }> = ({ chat_elem }) => {
 
   return (
     <>
-      <li key={chat.id} style={{ listStyle: "none" }}>
+      <li key={chat_elem.id} style={{ listStyle: "none" }}>
         <div className='user-name'>
-          {chat.use_name}
+          {chat_elem.user_name}
         </div>
         <div className="chat-content">
-          {chat.chat_content}
+          {chat_elem.chat_content}
         </div>
       </li>
       <hr />
@@ -65,19 +72,71 @@ const Home: NextPage<HomeProps> = ({ chat }) => {
       if (res.contents) {
         setChat(res.contents.reverse())
       }
-    }, 10000)
+    }, 2000)
     return () => clearInterval(intervalId)
-  }, [])
+  }, []);
+
+  const [userName, setName] = useState("");
+
+  const changeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }
+
+  const [inputContents, setContents] = useState("")
+
+  const changefield = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContents(e.target.value);
+  }
+
+  const contentSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!userName || !inputContents) return;
+
+    var submitContents: SubmitContents = {
+      user_name: userName,
+      chat_content: inputContents
+    };
+
+    try {
+      const res = await axios.post(
+        "https://chat.microcms.io/api/v1/chat",
+        JSON.stringify(submitContents),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            'X-API-KEY': process.env.API_KEY
+          },
+        }
+      )
+      if (res.status === 200 || res.status === 201) {
+        console.log("ok");
+      } else {
+        console.error(res.data);
+      }
+    } catch (error) {
+      console.error(error)
+    }
+    setName("");
+    setContents("");
+  }
 
   return (
     <main>
       <ul>
         {chat_contents.map((chat_content: Chat) => (
-          <Chat key={chat_content.id} chat={chat_content} />
+          <Chat key={chat_content.id} chat_elem={chat_content} />
         ))}
       </ul>
-      <form action="" method="post">
-        <input type="text" name="" id="" />
+      <form action="" method="post" onSubmit={(e) => { contentSubmit(e) }} >
+        <div className="user-name">
+          <h3>name</h3>
+          <input type="text" value={userName} name="name" id="name" onChange={(e) => { changeName(e) }} required />
+        </div>
+        <div className="chat-content">
+          <h3>本文</h3>
+          <textarea value={inputContents} name='chat_contents' id='chat_contents' onChange={(e) => { changefield(e) }} required />
+        </div>
+        <input type="submit" name='submit' />
       </form>
     </main>
   );
